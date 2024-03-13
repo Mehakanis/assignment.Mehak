41c5e27f1edf9c802e82f2479daee4a100d5098a
Mehakanis <159849439+Mehakanis@users.noreply.github.com>
2024-03-13 12:58:48 -0700

    Add files via upload

---
+++ objects.js
@@ -0,0 +1,35 @@
+// objects => {} => {key: value} pair
+// object array is used, if multiple students are there.
+var student = {
+    fullName: 'Mehak Anis',
+    classId: 2243,
+    rollNo: 1005,
+    email: 'abc@.gmail.com',
+    subjects: ['eng', 'urdu', 'maths'],
+    isPassed: true,
+    feeSchedule: {
+        month: 'march 2024',
+        fee: 10000,
+    }
+};
+//console.log(student);
+console.log(student.fullName);
+console.log(student.subjects[1]);
+console.log(student.isPassed);
+console.log(student.feeSchedule.month + ', Amount = ' + student.feeSchedule.fee);
+//[[],[],[]] array of array
+// { key: 'a', key2: []}
+// array ke andar koi bhi data type store ho sakti hai.
+// called as nested array
+// let object ={
+//     key:{ bookName: 'Harry Potter',
+//     author: 'J.K. Rowling', 
+//         JK_Rowling : {age:26,
+//         born:'london',
+//     }
+//     },
+//     places: ['abc','xyz'],
+// };
+//console.log(object);
+//console.log(object.places[0])
+//object ke andar ham array bhi bana sakte hain.
