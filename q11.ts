1eda24477d58856191c9016dbd571f8190fd339a
Mehakanis <159849439+Mehakanis@users.noreply.github.com>
2024-03-17 15:43:02 -0700

    Add files via upload

---
+++ q11.ts
@@ -0,0 +1,13 @@
+// Store the names of a few of your friends in a array called names.
+// Print each person’s name by accessing each element in the list, one 
+//at a time.
+
+let names: string[] =['Ayesha','Aimen','Zainab','Huda','Saira']
+//console.log(names[0]);
+//console.log(names[1]);
+//console.log(names[2]);
+//console.log(names[3]);
+//console.log(names[4]);
+for(let i=0; i<names.length; i++){ // short form , i++ mtlb loop continue rahe
+    console.log(names[i]);
+}
---
+++ q6.js
@@ -0,0 +1,12 @@
+var someone = "\n\tMehakanis\t\n";
+console.log(someone);
+var someone1 = "\nMehakanis\t";
+console.log(someone1);
+var someone2 = "\tMehakanis\t";
+console.log(someone2);
+var someone3 = "\tMehak\tanis";
+console.log(someone3);
+var someone4 = "Mehak \n anis";
+console.log(someone4);
+var striping = someone1.trim();
+console.log(striping);
